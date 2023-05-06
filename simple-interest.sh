1c4bd1fdd7a18b68e76df292aed699725babdafd
Bleach-375 <132614268+Bleach-375@users.noreply.github.com>
2023-05-06 13:37:21 -0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Bleach-375
    # Input:
    # p, principal amount
    # t, time period in years
